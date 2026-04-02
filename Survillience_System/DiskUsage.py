8dadc362198278420a00544da5a0a7fc0e0de4fc
Saurabh Ravindra Bhonsle <141349409+Saurabh-970@users.noreply.github.com>
2026-04-02 16:19:00 +0000

    Add files via upload

---
+++ Survillience_System/DiskUsage.py
@@ -0,0 +1,113 @@
+#Command  line input
+
+import psutil
+import sys
+import os
+import time
+import schedule
+
+######################### PROCESS AUTOMATION ##########################
+def CreateLog(FolderName):
+    Ret = False
+    Border = "-"*50
+
+    Ret = os.path.exists(FolderName)
+    if(Ret == True):
+        Ret = os.path.isdir(FolderName)
+        if(Ret == False):
+            print("Unable to create folder")
+    else:
+        os.mkdir(FolderName)
+        print("Directory for log files gets created successfully")
+
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")      #direct string bhette
+    FileName = os.path.join(FolderName,"Marvellous_%s.log" %timestamp)
+    print("Log file gets created with name : ",FileName)
+
+
+    fobj = open(FileName, "w")
+
+    fobj.write(Border+"\n")
+    fobj.write("--------- Platform Surveillance System---------")
+    fobj.write("Log created at : "+time.ctime()+"\n")
+    fobj.write(Border+"\n")
+    fobj.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+    fobj.write(Border+"\n")
+    fobj.write("----------------End of log file----------------")
+    fobj.write(Border+"\n")
+
+    print("CPU usage : ",psutil.cpu_percent())
+
+    Mem = psutil.virtual_memory()
+    print("RAM usage : ",Mem.percent)
+
+    for part in psutil.disk_partitions():
+        usage = psutil.disk_usage(part.mountpoint)  #kuthe mount zhalay tyacha nav
+        print(f"{part.mountpoint} used {usage.percent}%%")
+
+def main():
+    Border = "-"*50
+    print(Border)
+    print("---------- Platform Surveillance System---------")
+    print(Border)
+
+    if(len(sys.argv)==2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to : ")
+            print("1 : Create automatic logs")
+            print("2 : Executes Periodically")
+            print("3 : Sends mail with the log")
+            print("4 : Store infirmation about processes")
+            print("5 : Store the information about the CPU")
+            print("6 : Store the information about the RAM usage")
+            print("7 : Store the information about the secondary storage")
+
+
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Use the automation script as : ")
+            print("Scriptname.py TimeInterval Directoryname")
+            print("TimeInterval : The time in minutes for periodic scheduling")
+            print("DirectoryName : Name of directory to create auto logs")
+
+        else:
+            print("Unable to proceed as there is no such option")
+            print("Please use --h or --u to get more details")
+
+    #python Demo.py 5 Marvellous
+    elif(len(sys.argv)==3):
+        print("Inside projects logic")
+        print("TimeInterval : ",sys.argv[1])
+        print("DirectoryName : ",sys.argv[2])
+
+        #Apply the scheduler
+        schedule.every(int(sys.argv[1])).minutes.do(CreateLog, sys.argv[2])
+        
+        
+        print("------- Platform Surveillance System started successfully-----")
+        print("Directory created with name : ",sys.argv[2])
+        print("Time interval : ",sys.argv[1])
+        print("Press ctrl + c to stop the execution")
+
+        #wait till abort
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    else:
+        print("Invalid no of COMMAND LINE ARGUMENTS")
+        print("Unable to proceed as there is no such option")
+        print("Please use --h or --u to get more details")
+
+
+
+
+
+
+    print(Border)
+    print("----------Thankyou for using our script----------")
+    print(Border)
+    
+
+if __name__ == "__main__":
+    main()
