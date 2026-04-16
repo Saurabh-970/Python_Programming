1be4eeac403d3bbde19683a35fe5a8628bf8f0d9
Saurabh Ravindra Bhonsle <141349409+Saurabh-970@users.noreply.github.com>
2026-04-17 01:52:42 +0530

    Add files via upload

---
+++ Conceptual_Codes/Inheritance.py
@@ -0,0 +1,18 @@
+class Parent:
+    def __init__(self):
+        print("Inside parent constructor")
+        self.No1 = 10
+        self.No2 = 20
+
+    def fun(self):
+        print("Inside fun method of parent")
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        print("Inside Child constructor")
+        self.A =11
+        self.B =21
+
+    def sun(self):
+        print("inside sun method of child")
---
+++ Conceptual_Codes/InheritenceMultiple.py
@@ -0,0 +1,8 @@
+class Parent:
+    pass
+
+class Parent2:
+    pass
+
+class Child(Parent,Parent2):
+    pass
