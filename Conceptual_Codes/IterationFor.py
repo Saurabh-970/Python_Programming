beef8d280df74003b4a6cd94e43a047971d6c45a
Saurabh Ravindra Bhonsle <141349409+Saurabh-970@users.noreply.github.com>
2026-04-20 12:38:36 +0530

    Add files via upload

---
+++ Conceptual_Codes/IterationFor.py
@@ -0,0 +1,5 @@
+#Sequence
+
+print("1")
+print("2")
+print("3")
---
+++ Conceptual_Codes/IterationForXX.py
@@ -0,0 +1,9 @@
+# print("Jay Ganesh")
+# print("Jay Ganesh")
+# print("Jay Ganesh")
+# print("Jay Ganesh")
+
+
+#Iteration
+for i in range(4):
+   print("Jay Ganesh")
